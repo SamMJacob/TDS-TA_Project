daa8f9e41fec7325369ace73ebf70b1fd1291aeb
SamMJacob1 <sam@clearlyblue.in>
2025-06-14 09:29:55 +0530

    deployment setups

--- server/server/settings.py
+++ server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)6yca_k-2!lzvlpy-$$@j4^rt*q^u2^*zmxwrt7^=q+qxoism9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tds-ta-project.onrender.com',]
 
 
 
