a09f936b24605360c52b05146792a8f0fe96159d
SamMJacob1 <sam@clearlyblue.in>
2025-06-14 10:35:55 +0530

    handling allowed hosts and cors

--- server/server/urls.py
+++ server/server/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+from django.urls import path
 
 urlpatterns = [
+    path('', lambda request: JsonResponse({"status": "ok"})),
     path('admin/', admin.site.urls),
     path('api/', include('virtual_ta.urls')),
 ]
